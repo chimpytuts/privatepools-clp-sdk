17f63f22538d6995641c0a01a30674d5c984e7f5
Chimpy Developer <joaoc@d3vs.xyz>
2024-02-07 16:02:57 -0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
-export const FACTORY_ADDRESS = '0x1F98431c8aD98523631AE4a59f267346ea31F984'
+export const FACTORY_ADDRESS = '0xBA172a1feAC8479Ca986827100753d5D4C89D707'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
-export const POOL_INIT_CODE_HASH = '0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54'
+export const POOL_INIT_CODE_HASH = '0x6ce8eb472fa82df5469c6ab6d485f17c3ad13c8cd7af59b3d4a8026c5ce0f7e2'
 
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.
