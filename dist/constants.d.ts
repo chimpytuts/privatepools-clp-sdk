ed1bc0b7cc6057b843070d7e6d89d44de52c67a6
The Chimpy Tuts <team@d3vs.xyz>
2024-02-27 16:45:08 +0000

    Remove 'dist' directory from .gitignore

---
+++ dist/constants.d.ts
@@ -0,0 +1,18 @@
+export declare const FACTORY_ADDRESS = "0xFfDb6e87aA9B7425E542602C7Ea689D76182A9Ad";
+export declare const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
+export declare const POOL_INIT_CODE_HASH = "0x6ce8eb472fa82df5469c6ab6d485f17c3ad13c8cd7af59b3d4a8026c5ce0f7e2";
+/**
+ * The default factory enabled fee amounts, denominated in hundredths of bips.
+ */
+export declare enum FeeAmount {
+    LOWEST = 100,
+    LOW = 500,
+    MEDIUM = 3000,
+    HIGH = 10000
+}
+/**
+ * The default factory tick spacings by fee amount.
+ */
+export declare const TICK_SPACINGS: {
+    [amount in FeeAmount]: number;
+};
